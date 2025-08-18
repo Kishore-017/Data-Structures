eb8166b141d0986de90e66dedc1d048503d1bb14
Kishore-017 <kishoressenthil@gmail.com>
2025-08-18 14:39:49 +0530

    Add files via upload

---
+++ Ex_3/linked-list-imp/linked-list-imp.py
@@ -0,0 +1,39 @@
+class Node:
+    def __init__(self, data):
+        self.data = data
+        self.next = None
+
+class Stack:
+    def __init__(self):
+        self.top = None
+
+    def push(self, title):
+        new_node = Node(title)
+        new_node.next = self.top
+        self.top = new_node
+        print("Pushed:", title)
+
+    def pop(self):
+        if self.top is None:
+            print("Stack is empty")
+        else:
+            print("Popped:", self.top.data)
+            self.top = self.top.next
+
+    def display(self):
+        if self.top is None:
+            print("Stack is empty")
+        else:
+            tmp = self.top
+            while tmp:
+                print(tmp.data, end=" -> ")
+                tmp = tmp.next
+            print("Null")
+
+# Create and use the stack
+stack = Stack()
+stack.push("book A")
+stack.push("book B")
+stack.display()
+stack.pop()
+stack.display()
